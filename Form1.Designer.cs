5415c131a8e560375f3bf442154b47122937cc25
Paul TAITINGER <paul.taitinger@open-groupe.com>
2018-12-07 15:00:35 +0100

    update for CSMoneyCycleApp

---
+++ Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace AppCSEcoCycle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTeam = new System.Windows.Forms.Label();
+            this.lbIndiv = new System.Windows.Forms.Label();
+            this.btnT = new System.Windows.Forms.Button();
+            this.btnCT = new System.Windows.Forms.Button();
+            this.btnEco = new System.Windows.Forms.Button();
+            this.btnArme = new System.Windows.Forms.Button();
+            this.btnForce = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPlant = new System.Windows.Forms.Button();
+            this.btnDefuse = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnWin = new System.Windows.Forms.Button();
+            this.btnLose = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalculer = new System.Windows.Forms.Button();
+            this.btnRecommencer = new System.Windows.Forms.Button();
+            this.btnNoStuff = new System.Windows.Forms.Button();
+            this.btnMidStuff = new System.Windows.Forms.Button();
+            this.btnFullStuff = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTeam
+            // 
+            this.lbTeam.AutoSize = true;
+            this.lbTeam.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTeam.Location = new System.Drawing.Point(67, 92);
+            this.lbTeam.Name = "lbTeam";
+            this.lbTeam.Size = new System.Drawing.Size(164, 40);
+            this.lbTeam.TabIndex = 0;
+            this.lbTeam.Text = "unkwon $";
+            // 
+            // lbIndiv
+            // 
+            this.lbIndiv.AutoSize = true;
+            this.lbIndiv.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIndiv.Location = new System.Drawing.Point(382, 90);
+            this.lbIndiv.Name = "lbIndiv";
+            this.lbIndiv.Size = new System.Drawing.Size(112, 42);
+            this.lbIndiv.TabIndex = 1;
+            this.lbIndiv.Text = "800 $";
+            // 
+            // btnT
+            // 
+            this.btnT.BackColor = System.Drawing.Color.Red;
+            this.btnT.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnT.ForeColor = System.Drawing.Color.White;
+            this.btnT.Location = new System.Drawing.Point(160, 172);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(112, 46);
+            this.btnT.TabIndex = 2;
+            this.btnT.Text = "T";
+            this.btnT.UseVisualStyleBackColor = false;
+            this.btnT.Click += new System.EventHandler(this.btnTerroriste_Click);
+            // 
+            // btnCT
+            // 
+            this.btnCT.BackColor = System.Drawing.Color.Blue;
+            this.btnCT.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnCT.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCT.ForeColor = System.Drawing.Color.White;
+            this.btnCT.Location = new System.Drawing.Point(297, 172);
+            this.btnCT.Name = "btnCT";
+            this.btnCT.Size = new System.Drawing.Size(112, 46);
+            this.btnCT.TabIndex = 3;
+            this.btnCT.Text = "CT";
+            this.btnCT.UseVisualStyleBackColor = false;
+            this.btnCT.Click += new System.EventHandler(this.btnCouterTerroriste_Click);
+            // 
+            // btnEco
+            // 
+            this.btnEco.Location = new System.Drawing.Point(150, 240);
+            this.btnEco.Name = "btnEco";
+            this.btnEco.Size = new System.Drawing.Size(75, 23);
+            this.btnEco.TabIndex = 4;
+            this.btnEco.Text = "éco";
+            this.btnEco.UseVisualStyleBackColor = true;
+            this.btnEco.Click += new System.EventHandler(this.btnEco_Click);
+            // 
+            // btnArme
+            // 
+            this.btnArme.Location = new System.Drawing.Point(150, 341);
+            this.btnArme.Name = "btnArme";
+            this.btnArme.Size = new System.Drawing.Size(75, 23);
+            this.btnArme.TabIndex = 5;
+            this.btnArme.Text = "Armé";
+            this.btnArme.UseVisualStyleBackColor = true;
+            this.btnArme.Click += new System.EventHandler(this.btnArme_Click);
+            // 
+            // btnForce
+            // 
+            this.btnForce.Location = new System.Drawing.Point(150, 274);
+            this.btnForce.Name = "btnForce";
+            this.btnForce.Size = new System.Drawing.Size(75, 23);
+            this.btnForce.TabIndex = 6;
+            this.btnForce.Text = "Force Buy";
+            this.btnForce.UseVisualStyleBackColor = true;
+            this.btnForce.Click += new System.EventHandler(this.btnForce_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(234, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "+";
+            // 
+            // btnPlant
+            // 
+            this.btnPlant.Location = new System.Drawing.Point(276, 253);
+            this.btnPlant.Name = "btnPlant";
+            this.btnPlant.Size = new System.Drawing.Size(75, 23);
+            this.btnPlant.TabIndex = 8;
+            this.btnPlant.Text = "Plant";
+            this.btnPlant.UseVisualStyleBackColor = true;
+            this.btnPlant.Click += new System.EventHandler(this.btnPlant_Click);
+            // 
+            // btnDefuse
+            // 
+            this.btnDefuse.Location = new System.Drawing.Point(276, 309);
+            this.btnDefuse.Name = "btnDefuse";
+            this.btnDefuse.Size = new System.Drawing.Size(75, 23);
+            this.btnDefuse.TabIndex = 9;
+            this.btnDefuse.Text = "Defuse";
+            this.btnDefuse.UseVisualStyleBackColor = true;
+            this.btnDefuse.Click += new System.EventHandler(this.btnDefuse_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(326, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Moyenne Argent indiv :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(226, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Moyenne Argent équipe:";
+            // 
+            // btnWin
+            // 
+            this.btnWin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnWin.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWin.ForeColor = System.Drawing.Color.White;
+            this.btnWin.Location = new System.Drawing.Point(17, 253);
+            this.btnWin.Name = "btnWin";
+            this.btnWin.Size = new System.Drawing.Size(79, 33);
+            this.btnWin.TabIndex = 12;
+            this.btnWin.Text = "Win";
+            this.btnWin.UseVisualStyleBackColor = false;
+            this.btnWin.Click += new System.EventHandler(this.btnWin_Click);
+            // 
+            // btnLose
+            // 
+            this.btnLose.BackColor = System.Drawing.Color.Maroon;
+            this.btnLose.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLose.ForeColor = System.Drawing.Color.White;
+            this.btnLose.Location = new System.Drawing.Point(17, 302);
+            this.btnLose.Name = "btnLose";
+            this.btnLose.Size = new System.Drawing.Size(79, 33);
+            this.btnLose.TabIndex = 13;
+            this.btnLose.Text = "Lose";
+            this.btnLose.UseVisualStyleBackColor = false;
+            this.btnLose.Click += new System.EventHandler(this.btnLose_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(102, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 33);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "+";
+            // 
+            // btnCalculer
+            // 
+            this.btnCalculer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCalculer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculer.ForeColor = System.Drawing.Color.White;
+            this.btnCalculer.Location = new System.Drawing.Point(541, 274);
+            this.btnCalculer.Name = "btnCalculer";
+            this.btnCalculer.Size = new System.Drawing.Size(113, 33);
+            this.btnCalculer.TabIndex = 15;
+            this.btnCalculer.Text = "Calculer";
+            this.btnCalculer.UseVisualStyleBackColor = false;
+            this.btnCalculer.Click += new System.EventHandler(this.btnCalculer_Click);
+            // 
+            // btnRecommencer
+            // 
+            this.btnRecommencer.BackColor = System.Drawing.Color.Navy;
+            this.btnRecommencer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRecommencer.Location = new System.Drawing.Point(568, 342);
+            this.btnRecommencer.Name = "btnRecommencer";
+            this.btnRecommencer.Size = new System.Drawing.Size(86, 22);
+            this.btnRecommencer.TabIndex = 16;
+            this.btnRecommencer.Text = "Recommencer";
+            this.btnRecommencer.UseVisualStyleBackColor = false;
+            this.btnRecommencer.Click += new System.EventHandler(this.btnRecommencer_Click);
+            // 
+            // btnNoStuff
+            // 
+            this.btnNoStuff.Location = new System.Drawing.Point(421, 255);
+            this.btnNoStuff.Name = "btnNoStuff";
+            this.btnNoStuff.Size = new System.Drawing.Size(88, 25);
+            this.btnNoStuff.TabIndex = 17;
+            this.btnNoStuff.Text = "Aucun";
+            this.btnNoStuff.UseVisualStyleBackColor = true;
+            // 
+            // btnMidStuff
+            // 
+            this.btnMidStuff.Location = new System.Drawing.Point(421, 286);
+            this.btnMidStuff.Name = "btnMidStuff";
+            this.btnMidStuff.Size = new System.Drawing.Size(88, 22);
+            this.btnMidStuff.TabIndex = 18;
+            this.btnMidStuff.Text = "Un peu";
+            this.btnMidStuff.UseVisualStyleBackColor = true;
+            // 
+            // btnFullStuff
+            // 
+            this.btnFullStuff.Location = new System.Drawing.Point(421, 314);
+            this.btnFullStuff.Name = "btnFullStuff";
+            this.btnFullStuff.Size = new System.Drawing.Size(88, 23);
+            this.btnFullStuff.TabIndex = 19;
+            this.btnFullStuff.Text = "Full Stuff";
+            this.btnFullStuff.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(367, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 33);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(436, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Stuff ?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(2, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 16);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Choisie le Side Adverse :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 390);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFullStuff);
+            this.Controls.Add(this.btnMidStuff);
+            this.Controls.Add(this.btnNoStuff);
+            this.Controls.Add(this.btnRecommencer);
+            this.Controls.Add(this.btnCalculer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnLose);
+            this.Controls.Add(this.btnWin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDefuse);
+            this.Controls.Add(this.btnPlant);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnForce);
+            this.Controls.Add(this.btnArme);
+            this.Controls.Add(this.btnEco);
+            this.Controls.Add(this.btnCT);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.lbIndiv);
+            this.Controls.Add(this.lbTeam);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eco Cyclie App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTeam;
+        private System.Windows.Forms.Label lbIndiv;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Button btnCT;
+        private System.Windows.Forms.Button btnEco;
+        private System.Windows.Forms.Button btnArme;
+        private System.Windows.Forms.Button btnForce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPlant;
+        private System.Windows.Forms.Button btnDefuse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnWin;
+        private System.Windows.Forms.Button btnLose;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalculer;
+        private System.Windows.Forms.Button btnRecommencer;
+        private System.Windows.Forms.Button btnNoStuff;
+        private System.Windows.Forms.Button btnMidStuff;
+        private System.Windows.Forms.Button btnFullStuff;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCSEcoCycle
+{
+    public partial class Form1 : Form
+    {
+        //the basic money starter
+        int argentIndiv = 800;
+        int argentTeam ;
+
+        //detect if the enemy team is from CT or T
+        bool terroriste = false;
+        bool counterTerroriste = false;
+
+        //Usefull Variables for lose rounds 
+        int loseCount = 0;
+        int GRLoseAvailable = 1;
+        bool roundLose = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTerroriste_Click(object sender, EventArgs e)
+        {
+            terroriste = true;
+        }
+
+        private void btnCouterTerroriste_Click(object sender, EventArgs e)
+        {
+            counterTerroriste = true;
+        }
+
+        private void btnWin_Click(object sender, EventArgs e)
+        {
+            if(terroriste == true)
+            {
+                argentIndiv += 3250;
+                loseCount = 0;
+                GRLoseAvailable = 0;
+            }
+            else if(counterTerroriste == true)
+            {
+                argentIndiv += 3250;
+                loseCount = 0;
+                GRLoseAvailable = 0;
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnCalculer_Click(object sender, EventArgs e)
+        {
+            //Base money for ennemy team after round finish and sum was made
+            lbIndiv.Text = argentIndiv.ToString() + " $ ";
+            argentTeam = argentIndiv * 5;
+            lbTeam.Text = argentTeam.ToString() + " $ ";
+
+        }
+
+        private void btnLose_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                //Only if it's gunRound, so he is available
+                if (GRLoseAvailable == 1)
+                {
+                    if (loseCount == 0)
+                        argentIndiv += 1900;
+                    if (loseCount == 1)
+                        argentIndiv += 2400;
+                    if (loseCount == 2)
+                        argentIndiv += 2900;
+                    if (loseCount == 3)
+                        argentIndiv += 3400;
+                    loseCount++;
+                }
+                else
+                {
+                    if (loseCount == 0)
+                        argentIndiv += 1400;
+                    if (loseCount == 1)
+                        argentIndiv += 1900;
+                    if (loseCount == 2)
+                        argentIndiv += 2400;
+                    if (loseCount == 3)
+                        argentIndiv += 2900;
+                    if (loseCount >= 4)
+                        argentIndiv += 3400;
+                    loseCount++;
+                }
+                //connected to bomb plant, if the round lose is deteted by true and plant +800$
+                roundLose = true;
+            }
+            else if (counterTerroriste == true)
+            {
+                //Only if it's gunRound, so he is available
+                if (GRLoseAvailable == 1)
+                {
+                    if (loseCount == 0)
+                        argentIndiv += 1900;
+                    if (loseCount == 1)
+                        argentIndiv += 2400;
+                    if (loseCount == 2)
+                        argentIndiv += 2900;
+                    if (loseCount == 3)
+                        argentIndiv += 3400;
+                    loseCount++;
+                }
+                else
+                {
+                    if (loseCount == 0)
+                        argentIndiv += 1400;
+                    if (loseCount == 1)
+                        argentIndiv += 1900;
+                    if (loseCount == 2)
+                        argentIndiv += 2400;
+                    if (loseCount == 3)
+                        argentIndiv += 2900;
+                    if (loseCount >= 4)
+                        argentIndiv += 3400;
+                    loseCount++;
+                }
+                //connected to bomb plant, if the round lose is deteted by true and plant +800$
+                roundLose = true;
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnEco_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                argentIndiv -= 700;
+            }
+            else if (counterTerroriste == true)
+            {
+                argentIndiv -= 700;
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnArme_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                argentIndiv -= 3700;
+            }
+            else if (counterTerroriste == true)
+            {
+                argentIndiv -= 4100;
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnForce_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                argentIndiv -= 2300;
+            }
+            else if (counterTerroriste == true)
+            {
+                argentIndiv -= 2300;
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnPlant_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                if(roundLose == true)
+                {
+                    argentIndiv += 800;
+                    roundLose = false;
+                }
+                else
+                {
+                    //Approximativ average
+                    argentIndiv += 250;
+                }
+            }
+            else if (counterTerroriste == true)
+            {
+                MessageBox.Show("Les CT ne plante pas de bombe :/ (change de side)");
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnDefuse_Click(object sender, EventArgs e)
+        {
+            if (terroriste == true)
+            {
+                MessageBox.Show("Les terroristes ne defuse pas de bombe (change de side)");
+            }
+            else if (counterTerroriste == true)
+            {
+                if(roundLose == true)
+                {
+                    MessageBox.Show("Les CT ne peuvent pas perdre le round en désamorçant");
+                }
+                else
+                {
+                    argentIndiv += 250;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choisie un Side adverse avant :) ");
+            }
+        }
+
+        private void btnRecommencer_Click(object sender, EventArgs e)
+        {
+            /*  --- We set all Variables to default --- */
+            /* --- for a clean restart or change side --- */
+
+            argentIndiv = 800;
+
+            //detect if the enemy team is from CT or T
+            terroriste = false;
+            counterTerroriste = false;
+
+            //Usefull Variables for lose rounds 
+            loseCount = 0;
+            GRLoseAvailable = 1;
+            roundLose = false;
+
+            lbIndiv.Text = argentIndiv.ToString() + " $ ";
+            argentTeam = argentIndiv * 5;
+            lbTeam.Text = argentTeam.ToString() + " $ ";
+        }
+    }
+}
